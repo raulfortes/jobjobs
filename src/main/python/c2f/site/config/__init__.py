5d63a9ad79a924dbe8c6ffcee0902140e3c16ddd
Raul Fortes <raulfortes@gmail.com>
2012-03-23 22:24:10 -0300

    init

---
+++ src/main/python/c2f/site/config/__init__.py
@@ -0,0 +1,14 @@
+import ConfigParser
+from logging.config import fileConfig as log_config_read
+
+base_path = '.'
+config_file = 'system.conf'
+parameters = ConfigParser.RawConfigParser()
+
+def load(filename=None):
+    if filename != None:
+        config_file = filename
+    
+    #Load or reload config
+    parameters.read(config_file)
+    log_config_read(config_file)
